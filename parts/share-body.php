dbca479159def7680f85e2825ac08d54c5fac7c1
yat8823jp <yat@yat-net.com>
2017-12-20 21:32:17 +0900

    change-tw-share

--- parts/share-body.php
+++ parts/share-body.php
@@ -9,7 +9,7 @@
 		</a>
 	</li>
 	<li class="hateb metasns-item">
-		<a href="//twitter.com/share/?text=<?php the_title(); ?>:YATのBlog&amp;url=<?php the_permalink(); ?>"  onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;" rel="nofollow">
+		<a href="//twitter.com/share/?text=<?php echo esc_html( get_the_title() ); ?>:YATのBlog&amp;url=<?php the_permalink(); ?>"  onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;" rel="nofollow">
 			<dl class="metasns-item-set">
 				<dt><img src="<?php echo COMMON_PFIX . '/img/share/twitter_bt.png' ?>" alt="ツイッターでつぶやく" class="metasns-tw"></dt>
 				<dd><?php if(! function_exists('scc_get_share_twitter()')) echo scc_get_share_twitter(); ?></dd>
